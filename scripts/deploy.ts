88890b6b5cd59d807c0c692b9a9ff3f8f6ec2b49
Maxim280596 <max280596@gmail.com>
2024-01-09 13:58:41 +0200

    initial commit

---
+++ scripts/deploy.ts
@@ -0,0 +1,11 @@
+import { ethers } from "hardhat";
+import { verifyContract } from "./verify";
+
+async function main() {
+  
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
